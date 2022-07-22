8f8107c37d35ec94960e38295f81516fb3f03862
Calvin-alis <alis.demiurg@gmail.com>
2022-07-22 12:12:47 +0300

    add start lesson-3 file

---
+++ lesson-3.sql
@@ -0,0 +1,31 @@
+# Lesson 3
+
+/* 
+Slide 8
+Task 1: Select all employees whose average salary is higher than $120,000 per  annum.
+*/
+
+
+/*
+Slide 11-12
+Task 1: Select the employee numbers of all individuals who have signed  more than 1 contract after the 1st of January 2000.
+Task 2: What is the total amount of money spent on salaries for all contracts  starting after the 1st of January 1997?
+*/
+
+
+/* 
+Slide 13
+Task 1: Which is the lowest employee number in the database?
+Task 2: Which is the highest employee number in the database?
+*/
+
+/*
+Slide 15
+Task 1: Show average annual salaries that are above 100k USD paid to  employees who started after the 1st of January 1997?
+*/
+
+
+/*
+Slide 17
+Task 1: Round the average amount of money spent on salaries for all contracts  that started after the 1st of January 1997 to a precision of cents.
+*/
